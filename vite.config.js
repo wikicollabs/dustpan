6fa88500f51c24ba1df2976d753beeed12bf4975
Raisha Abdillah <noncesura@gmail.com>
2026-07-30 11:05:46 +0700

    Update header to include SPDX license identifier

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Dustpan
+ * A tool to uncover WikiProjects that can be improved on Wikidata
+ * @see https://github.com/wikicollabs/dustpan
+ */
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
